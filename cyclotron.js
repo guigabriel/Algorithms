5fa0ad81ee2b05ba9a3892280c1b449d9476a782
guigabriel <guigabriel@mailfence.com>
2023-07-18 03:05:29 -0300

    Feat: create particle object

--- cyclotron.js
+++ cyclotron.js
@@ -5,3 +5,34 @@ function initializeMatrix(matrixSize) {
     }
     return matrix;
 }
+
+const particleConfig = {
+    '': (matrix, matrixSize) => {
+      for (let i = 0; i < matrixSize; i++) {
+        matrix[i][i] = 1;
+      }
+    },
+    'e': (matrix, matrixSize) => {
+      for (let i = 0; i < matrixSize; i++) {
+        matrix[0][i] = "e";
+        matrix[i][matrixSize - 1] = "e";
+      }
+    },
+    'p': (matrix, matrixSize) => {
+      for (let i = 0; i < matrixSize; i++) {
+        matrix[0][i] = "p";
+        matrix[i][0] = "p";
+        matrix[matrixSize - 1][i] = "p";
+        matrix[i][matrixSize - 1] = "p";
+        matrix[matrixSize - 2][matrixSize - 2] = "p";
+        matrix[matrixSize - 1][matrixSize - 1] = 1;
+      }
+    },
+    'n': (matrix, matrixSize) => {
+      for (let i = 0; i < matrixSize; i++) {
+        matrix[0][i] = "n";
+      }
+    }
+  };
+
+  
