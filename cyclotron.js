a210c8bd10d4880dee2d85bb07a36e1fad31a7fc
guigabriel <guigabriel@mailfence.com>
2023-07-18 03:13:51 -0300

    Fix: Formatter

--- cyclotron.js
+++ cyclotron.js
@@ -1,38 +1,36 @@
 function initializeMatrix(matrixSize) {
-    const matrix = []
-    for(let i = 0; i < matrixSize; i += 1){
-        matrix[i] = Array(matrixSize).fill(1);
-    }
-    return matrix;
+  const matrix = [];
+  for (let i = 0; i < matrixSize; i += 1) {
+    matrix[i] = Array(matrixSize).fill(1);
+  }
+  return matrix;
 }
 
 const particleConfig = {
-    '': (matrix, matrixSize) => {
-      for (let i = 0; i < matrixSize; i++) {
-        matrix[i][i] = 1;
-      }
-    },
-    'e': (matrix, matrixSize) => {
-      for (let i = 0; i < matrixSize; i++) {
-        matrix[0][i] = "e";
-        matrix[i][matrixSize - 1] = "e";
-      }
-    },
-    'p': (matrix, matrixSize) => {
-      for (let i = 0; i < matrixSize; i++) {
-        matrix[0][i] = "p";
-        matrix[i][0] = "p";
-        matrix[matrixSize - 1][i] = "p";
-        matrix[i][matrixSize - 1] = "p";
-        matrix[matrixSize - 2][matrixSize - 2] = "p";
-        matrix[matrixSize - 1][matrixSize - 1] = 1;
-      }
-    },
-    'n': (matrix, matrixSize) => {
-      for (let i = 0; i < matrixSize; i++) {
-        matrix[0][i] = "n";
-      }
+  "": (matrix, matrixSize) => {
+    for (let i = 0; i < matrixSize; i++) {
+      matrix[i][i] = 1;
     }
-  };
-
-  
+  },
+  e: (matrix, matrixSize) => {
+    for (let i = 0; i < matrixSize; i++) {
+      matrix[0][i] = "e";
+      matrix[i][matrixSize - 1] = "e";
+    }
+  },
+  p: (matrix, matrixSize) => {
+    for (let i = 0; i < matrixSize; i++) {
+      matrix[0][i] = "p";
+      matrix[i][0] = "p";
+      matrix[matrixSize - 1][i] = "p";
+      matrix[i][matrixSize - 1] = "p";
+      matrix[matrixSize - 2][matrixSize - 2] = "p";
+      matrix[matrixSize - 1][matrixSize - 1] = 1;
+    }
+  },
+  n: (matrix, matrixSize) => {
+    for (let i = 0; i < matrixSize; i++) {
+      matrix[0][i] = "n";
+    }
+  },
+};
